c370d35deb596e141761d4ab908146b03a6f2439
TrevorH-M <trevor@tatango.com>
2023-12-11 20:49:53 -0800

    Add more sytaxing, sorting, and a tad more style

--- RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/Models/SeedData.cs
@@ -14,32 +14,20 @@ namespace RazorPagesMovie.Models
                     throw new ArgumentNullException("Null RazorPagesMovieContext");
                 }
 
-                // Look for any Movies
-                if (context.Movie.Any()) 
-                {
-                    return; //DB has already been seeded
-                }
+                var movies = new List<Movie>();
 
-                context.Movie.AddRange(
+                movies.AddRange( new Movie[] {
                     new Movie { Title = "Inception", ReleaseDate = DateTime.Parse("2010-07-16"), Genre = "Sci-Fi", Price = 12.99M, Rating = "PG-13" },
                     new Movie { Title = "The Dark Knight", ReleaseDate = DateTime.Parse("2008-07-18"), Genre = "Action", Price = 11.99M, Rating = "PG-13" },
                     new Movie { Title = "Casablanca", ReleaseDate = DateTime.Parse("1942-11-26"), Genre = "Drama", Price = 6.99M, Rating = "PG" },
                     new Movie { Title = "Pulp Fiction", ReleaseDate = DateTime.Parse("1994-10-14"), Genre = "Crime", Price = 8.99M, Rating = "R" },
                     new Movie { Title = "The Matrix", ReleaseDate = DateTime.Parse("1999-03-31"), Genre = "Sci-Fi", Price = 10.99M, Rating = "R" },
-                    new Movie { Title = "Forrest Gump", ReleaseDate = DateTime.Parse("1994-07-06"), Genre = "Drama", Price = 9.99M, Rating = "PG-13" },
-                    new Movie { Title = "The Shawshank Redemption", ReleaseDate = DateTime.Parse("1994-09-10"), Genre = "Drama", Price = 9.99M, Rating = "R" },
                     new Movie { Title = "The Godfather", ReleaseDate = DateTime.Parse("1972-03-24"), Genre = "Crime", Price = 10.99M, Rating = "R" },
-                    new Movie { Title = "Schindler's List", ReleaseDate = DateTime.Parse("1993-12-15"), Genre = "Biography", Price = 11.99M, Rating = "R" },
-                    new Movie { Title = "The Silence of the Lambs", ReleaseDate = DateTime.Parse("1991-02-14"), Genre = "Crime", Price = 8.99M, Rating = "R" },
-                    new Movie { Title = "The Lord of the Rings: The Fellowship of the Ring", ReleaseDate = DateTime.Parse("2001-12-19"), Genre = "Adventure", Price = 12.99M, Rating = "PG-13" },
                     new Movie { Title = "Inglourious Basterds", ReleaseDate = DateTime.Parse("2009-08-21"), Genre = "Drama", Price = 8.99M, Rating = "R" },
-                    new Movie { Title = "The Departed", ReleaseDate = DateTime.Parse("2006-10-06"), Genre = "Crime", Price = 10.99M, Rating = "R" },
                     new Movie { Title = "Fight Club", ReleaseDate = DateTime.Parse("1999-10-15"), Genre = "Drama", Price = 9.99M, Rating = "R" },
                     new Movie { Title = "The Great Gatsby", ReleaseDate = DateTime.Parse("2013-05-10"), Genre = "Drama", Price = 11.99M, Rating = "PG-13" },
                     new Movie { Title = "Titanic", ReleaseDate = DateTime.Parse("1997-12-19"), Genre = "Romance", Price = 10.99M, Rating = "PG-13" },
-                    new Movie { Title = "Avatar", ReleaseDate = DateTime.Parse("2009-12-18"), Genre = "Action", Price = 12.99M, Rating = "PG-13" },
                     new Movie { Title = "Braveheart", ReleaseDate = DateTime.Parse("1995-05-24"), Genre = "Biography", Price = 9.99M, Rating = "R" },
-                    new Movie { Title = "The Green Mile", ReleaseDate = DateTime.Parse("1999-12-10"), Genre = "Crime", Price = 11.99M, Rating = "R" },
                     new Movie { Title = "Eternal Sunshine of the Spotless Mind", ReleaseDate = DateTime.Parse("2004-03-19"), Genre = "Drama", Price = 8.99M, Rating = "R" },
                     new Movie { Title = "The Godfather: Part II", ReleaseDate = DateTime.Parse("1974-12-20"), Genre = "Crime", Price = 11.99M, Rating = "R" },
                     new Movie { Title = "The Shawshank Redemption", ReleaseDate = DateTime.Parse("1994-09-10"), Genre = "Drama", Price = 9.99M, Rating = "R" },
@@ -54,7 +42,6 @@ namespace RazorPagesMovie.Models
                     new Movie { Title = "The Social Network", ReleaseDate = DateTime.Parse("2010-10-01"), Genre = "Biography", Price = 9.99M, Rating = "PG-13" },
                     new Movie { Title = "Forrest Gump", ReleaseDate = DateTime.Parse("1994-07-06"), Genre = "Drama", Price = 9.99M, Rating = "PG-13" },
                     new Movie { Title = "The Silence of the Lambs", ReleaseDate = DateTime.Parse("1991-02-14"), Genre = "Crime", Price = 8.99M, Rating = "R" },
-                    new Movie { Title = "The Great Gatsby", ReleaseDate = DateTime.Parse("2013-05-10"), Genre = "Drama", Price = 11.99M, Rating = "PG-13" },
                     new Movie { Title = "La La Land", ReleaseDate = DateTime.Parse("2016-12-09"), Genre = "Drama", Price = 10.99M, Rating = "PG-13" },
                     new Movie { Title = "The Grand Budapest Hotel", ReleaseDate = DateTime.Parse("2014-03-28"), Genre = "Adventure", Price = 9.99M, Rating = "R" },
                     new Movie { Title = "A Beautiful Mind", ReleaseDate = DateTime.Parse("2001-12-21"), Genre = "Biography", Price = 10.99M, Rating = "PG-13" },
@@ -80,9 +67,21 @@ namespace RazorPagesMovie.Models
                     new Movie { Title = "The Sixth Sense", ReleaseDate = DateTime.Parse("1999-08-06"), Genre = "Drama", Price = 9.99M, Rating = "PG-13" },
                     new Movie { Title = "The Exorcist", ReleaseDate = DateTime.Parse("1973-12-26"), Genre = "Horror", Price = 10.99M, Rating = "R" },
                     new Movie { Title = "Rocky", ReleaseDate = DateTime.Parse("1976-11-21"), Genre = "Drama", Price = 9.99M, Rating = "PG" },
-                    new Movie { Title = "The Sound of Music", ReleaseDate = DateTime.Parse("1965-03-02"), Genre = "Drama", Price = 9.99M, Rating = "G" }
-                );
-                context.SaveChanges();
+                    new Movie { Title = "The Sound of Music", ReleaseDate = DateTime.Parse("1965-03-02"), Genre = "Drama", Price = 9.99M, Rating = "G" },
+                    new Movie { Title = "The Lord of the Rings: The Fellowship of the Ring", ReleaseDate = DateTime.Parse("2001-12-19"), Genre = "Fantasy", Price = 14.99M, Rating = "PG-13" },
+                    new Movie { Title = "The Lord of the Rings: The Two Towers", ReleaseDate = DateTime.Parse("2002-12-18"), Genre = "Fantasy", Price = 14.99M, Rating = "PG-13" },
+                    new Movie { Title = "The Lord of the Rings: The Return of the King", ReleaseDate = DateTime.Parse("2003-12-17"), Genre = "Fantasy", Price = 14.99M, Rating = "PG-13" },
+                    new Movie { Title = "The Hobbit: An Unexpected Journey", ReleaseDate = DateTime.Parse("2012-12-14"), Genre = "Fantasy", Price = 12.99M, Rating = "PG-13" },
+                    new Movie { Title = "The Hobbit: The Desolation of Smaug", ReleaseDate = DateTime.Parse("2013-12-13"), Genre = "Fantasy", Price = 12.99M, Rating = "PG-13" },
+                    new Movie { Title = "The Hobbit: The Battle of the Five Armies", ReleaseDate = DateTime.Parse("2014-12-17"), Genre = "Fantasy", Price = 12.99M, Rating = "PG-13" }
+                });
+
+                if (context.Movie.Count() != movies.Count())
+                {
+                    context.Movie.RemoveRange(context.Movie);
+                    context.Movie.AddRange(movies);
+                    context.SaveChanges();
+                }
             }
         }
     }
--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -27,15 +27,34 @@ namespace RazorPagesMovie.Pages.Movies
 
         public SelectList? Genres { get; set; }
 
+        public SelectList? Ratings { get; set; }
+
         [BindProperty(SupportsGet = true)]
         public string? MovieGenre { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? MovieRating { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public decimal? MinPrice { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public decimal? MaxPrice { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime? MinReleaseDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime? MaxReleaseDate { get; set; }
 
         public async Task OnGetAsync()
         {
             // Use LINQ to get the list of genres.
             IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+            IQueryable<string> ratingQuery = from m in _context.Movie orderby m.Rating select m.Rating;
 
             var movies = from m in _context.Movie select m;
+
+
 
             if (!string.IsNullOrEmpty(SearchString))
             {
@@ -46,7 +65,33 @@ namespace RazorPagesMovie.Pages.Movies
             {
                 movies = movies.Where(x => x.Genre == MovieGenre);
             }
+
+            if (!string.IsNullOrEmpty(MovieRating))
+            {
+                movies = movies.Where(x => x.Rating == MovieRating);
+            }
+            if (MinPrice.HasValue)
+            {
+                movies = movies.Where(x => x.Price >= MinPrice.Value);
+            }
+
+            if (MaxPrice.HasValue)
+            {
+                movies = movies.Where(x => x.Price <= MaxPrice.Value);
+            }
+
+            if (MinReleaseDate.HasValue)
+            {
+                movies = movies.Where(m => m.ReleaseDate >= MinReleaseDate);
+            }
+
+            if (MaxReleaseDate.HasValue)
+            {
+                movies = movies.Where(m => m.ReleaseDate <= MaxReleaseDate);
+            }
+
             Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Ratings = new SelectList(await ratingQuery.Distinct().ToListAsync());
             Movie = await movies.ToListAsync();
         }
     }
